3c53bcd1ffad7320b80e31443ff5ec87b0507939
David Sanders <david.wm.sanders@gmail.com>
2021-01-24 10:32:12 +0000

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'curiousksp'
 year = '2021'
 author = 'David William Morris Sanders'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '0.1.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='curiousksp',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='ksp chaos with curio coroutines',
     long_description='%s\n%s' % (
